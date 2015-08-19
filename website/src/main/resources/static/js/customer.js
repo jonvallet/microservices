fe2b61d8ce0473502ef703029f95878a7d24c182
jon.vallet <jon.vallet@nakedwines.com>
2015-08-19 15:02:56 +0100

    Customers service now uses a postgresql database at runtime

--- website/src/main/resources/static/js/customer.js
+++ website/src/main/resources/static/js/customer.js
@@ -2,23 +2,35 @@ var customer = angular.module('customer', []);
 
 customer.controller('customerCtrl', function ($scope, $http) {
 
-    $scope.register = function (data) {
+    $scope.register = function (data, retry) {
+         retry = retry || 0
+         resetMessages()
+         $scope.show_registering = true
          $http.post('/customers/register', data)
              .error(function(error){
                 console.log(error)
-                $scope.show_success = false
-
                 if (error.error === "Conflict"){
+                    $scope.show_registering = false
                     $scope.show_conflict = true
-                }else {
+                }else if(retry < 3) {
+                    console.log("Retrying...")
+                    $scope.register(data, retry + 1)
+                } else {
+                    $scope.show_registering = false
                     $scope.show_error = true
                 }
              })
              .success(function(data) {
                     console.log(data)
+                    $scope.show_registering = false
                     $scope.show_success = true
-                    $scope.show_error = false
-                    $scope.show_conflict = false
              })
+    }
+
+    function resetMessages() {
+        $scope.show_success = false
+        $scope.show_error = false
+        $scope.show_conflict = false
+        $scope.show_registering = false
     }
 });
